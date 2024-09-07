878cc2b84907eba3a15dcf59f9b62328f93e7ddc
parikshit gaikwad <parikshitgaikwad172@gmail.com>
2024-09-07 11:25:06 +0530

    Fixes issue #1

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
         <Routing />
+        <h1 className='flex justify-center text-2xl transition ease-in-out delay-150  hover:scale-110 hover:text-3xl duration-300 '>Created with ❤️ Aniket </h1>
     </>
   )
 }
